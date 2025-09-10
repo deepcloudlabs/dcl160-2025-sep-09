ecb404e5aa8dda071cd45221403249c50855b7ee
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-10 09:49:49 +0300

    Create exercise01.py

---
+++ module02/exercises/exercise01.py
@@ -0,0 +1,5 @@
+languages_I_speak = ("English", "German", "Italian", "Polish", "Greek")
+print(languages_I_speak[2]) # Italian
+print(languages_I_speak[2:])
+languages_I_speak.append("Arabic") # error: AttributeError: 'tuple' object has no attribute 'append'
+
