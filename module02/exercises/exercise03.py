c7a666f846b3615efa0e84a9d2d2a2117361f7f8
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-10 10:05:36 +0300

    Create exercise03.py

---
+++ module02/exercises/exercise03.py
@@ -0,0 +1,12 @@
+my_dears = ["Werner", "Alex", "Anne-Flore", "Irene", "Alex"]
+
+# write the relevant Python code to sort the content in reverse-alphabetical order without creating another data structure
+
+# write the relevant Python code to look for ‘Werner’ value index
+
+# Define a strategy and write the Python code to cast my_dears list into a new data structure named unique_dears that only consists of unique values
+# list -> list/unique
+
+# write the relevant Python code in order to get the following result from the data structure unique_dears :
+# [(0, 'Anne-Flore'), (1, 'Werner'), (2, 'Irene'), (3, 'Alex')]
+
