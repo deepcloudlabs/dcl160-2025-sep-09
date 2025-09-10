1e95ea8dbc742be572f2aa3163a0f627d27ac817
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-10 15:52:27 +0300

    Update exercise05.py

--- module04/loops/exercise05.py
+++ module04/loops/exercise05.py
@@ -6,4 +6,15 @@
  3 digits design an algorithm and write a Python script
  to find his house number.
 """
-for i in range(100, 1000): # 100 ~ 999
+for building_no in range(100, 1000): # 100 ~ 999
+    left_sum = 0
+    for house_no in range(1, building_no):
+        left_sum += house_no
+    right_sum = 0
+    right_building_no = building_no + 1
+    while right_sum < left_sum:
+        right_sum += right_building_no
+        right_building_no += 1
+    if left_sum == right_sum:
+        print(f"Mathematician lives at {building_no}, and there are {right_building_no-1} buildings in the street!")
+        break
