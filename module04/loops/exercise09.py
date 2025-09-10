3db1a2a59404370b9594c096f95159bb66257e18
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-10 16:20:40 +0300

    Update exercise09.py

--- module04/loops/exercise09.py
+++ module04/loops/exercise09.py
@@ -7,4 +7,9 @@ employees = [
     ("sun kwon", "IT", 170000, 1984,False),
     ("hugo reyes", "IT", 120000, 1992,True)
 ]
-part_time_employees = [employee for employee in employees if ]
+part_time_employees = [employee for employee in employees if not employee[-1] ]
+full_time_employees = [(full_name,full_time) for full_name,dept,salary,birth_year,full_time in employees if full_time ]
+it_employees = [employee for employee in employees if employee[1].upper() == "IT"]
+print(part_time_employees)
+print(full_time_employees)
+print(it_employees)
