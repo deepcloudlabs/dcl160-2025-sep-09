df550d07fcecbebf6063a63f64d113b66a12f177
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-10 09:49:51 +0300

    Create exercise02.py

---
+++ module02/exercises/exercise02.py
@@ -0,0 +1,10 @@
+# Create a list named numbers consisting of integers from 0 to 10 (10 not included).
+
+print(numbers)
+# Add values of 35 and 48 to numbers in one command using a relevant function
+
+print(numbers)
+
+# Remove the last 4 values in the list by assigning a sliced empty list to numbers list
+print(numbers)
+
