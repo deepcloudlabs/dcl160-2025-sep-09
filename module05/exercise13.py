a405716827f90933f9e61903c61cc6cff234a658
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-11 10:59:52 +0300

    Create exercise13.py

---
+++ module05/exercise13.py
@@ -0,0 +1,22 @@
+import random
+
+def create_random_digit(min:int, max:int) -> int:
+    return random.randint(min, max)
+
+def create_secret_number(level : int) -> int:
+    digits = [create_random_digit(1,9)]
+    while (len(digits) < level):
+        digit = create_random_digit(0,9)
+        if digit not in digits:
+            digits.append(digit)
+    # [5, 4, 9] -> 549
+    secret = 0
+    for digit in digits:
+        secret = 10 * secret + digit
+    return secret
+
+print(create_secret_number(3))
+print(create_secret_number(3))
+print(create_secret_number(3))
+print(create_secret_number(3))
+print(create_secret_number(3))
