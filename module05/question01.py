e358a00da1cabbd525e90d7f7043fed6a6e6fd5e
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-11 10:39:52 +0300

    Create question01.py

---
+++ module05/question01.py
@@ -0,0 +1,13 @@
+"""
+Mastermind Game
+secret: 459
+level : 3 -> 10
+max moves: 10 -> 12 -> 14
+player: 123 -> No match
+        456 -> -2
+        574 -> -1+1
+        548 -> +2
+        549 -> level=4
+secret: 3615
+        1234 -> -2
+"""
