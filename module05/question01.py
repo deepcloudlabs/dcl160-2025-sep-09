c0da15478dbd75fe44a15fdc6449cd46e4f8fe6c
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-11 11:02:43 +0300

    Update question01.py

--- module05/question01.py
+++ module05/question01.py
@@ -11,3 +11,22 @@ player: 123 -> No match
 secret: 3615
         1234 -> -2
 """
+
+#region create secret
+import random
+def create_random_digit(min:int, max:int) -> int:
+    return random.randint(min, max)
+
+def create_secret_number(level : int) -> int:
+    digits = [create_random_digit(1,9)]
+    while (len(digits) < level):
+        digit = create_random_digit(0,9)
+        if digit not in digits:
+            digits.append(digit)
+    # [5, 4, 9] -> 549
+    secret = 0
+    for digit in digits:
+        secret = 10 * secret + digit
+    return secret
+#endregion
+
