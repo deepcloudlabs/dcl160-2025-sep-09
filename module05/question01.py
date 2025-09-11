0eaadcb734465db997423e31786d33badb31c723
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-11 12:11:38 +0300

    Update question01.py

--- module05/question01.py
+++ module05/question01.py
@@ -54,12 +54,34 @@ def move_to_next_level(state: dict) -> bool:
     state["max_moves"] += 2
     return True
 
+
 """
   12:00
   create_move(584,549) -> (587,"-1+1")
 """
+
+
 def create_move(guess: int, secret: int) -> tuple[int, str]:
-    pass
+    # int -> str
+    guess_str = str(guess)
+    secret_str = str(secret)
+    perfect_match = 0
+    partial_match = 0
+    for i, digit_of_guess in enumerate(guess_str):
+        for j, digit_of_secret in enumerate(secret_str):
+            if digit_of_guess == digit_of_secret:
+                if i == j:
+                    perfect_match += 1
+                else:
+                    partial_match += 1
+    if perfect_match == 0 and partial_match == 0:
+        return (guess, "No match")
+    message = ""
+    if partial_match > 0:
+        message = f"-{partial_match}"
+    if perfect_match > 0:
+        message = f"{message}+{perfect_match}"
+    return (guess, message)
 
 
 def game_loop(state=game_state):
